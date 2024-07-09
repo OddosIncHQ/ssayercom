3020ad8feefb1da7f7b5a4aab931284f1efd45c6
Oddos Inc. HQ <145717033+OddosIncHQ@users.noreply.github.com>
2024-07-09 01:13:03 -0400

    Delete website_formio directory

--- website_formio/CHANGELOG.md
+++
@@ -1,9 +0,0 @@
-# Changelog
-
-## 17.0.1.1
-
-Remove non-migrated website snippet.
-
-## 17.0.1.0
-
-Initial release.
--- website_formio/controllers/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright Nova Code (https://www.novacode.nl)
-# See LICENSE file for full licensing details.
-
-from . import main
