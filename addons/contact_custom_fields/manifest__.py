3c826fd3db9ca84d81e230109d270ebce75daee3
Oddos Inc. HQ <145717033+OddosIncHQ@users.noreply.github.com>
2024-07-10 03:04:47 -0400

    Update manifest__.py

--- addons/contact_custom_fields/manifest__.py
+++ addons/contact_custom_fields/manifest__.py
@@ -1,1 +1,10 @@
-
+{
+    'name': 'Contact Custom Fields',
+    'version': '1.0',
+    'category': 'Contacts',
+    'summary': 'Add custom fields to contacts',
+    'depends': ['base', 'contacts'],
+    'data': [
+        'views/res_partner_views.xml',
+    ],
+}
