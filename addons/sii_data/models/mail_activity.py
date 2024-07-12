4d112dbb33bdede62b9a0cfd29aae1d9d0455968
Oddos Inc. HQ <145717033+OddosIncHQ@users.noreply.github.com>
2024-07-12 00:44:55 -0400

    Update mail_activity.py

--- addons/sii_data/models/mail_activity.py
+++ addons/sii_data/models/mail_activity.py
@@ -1,6 +1,9 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class MailActivity(models.Model):
     _inherit = 'mail.activity'
 
-    activity_date_deadline = fields.Date(string='Activity Deadline')
+    activity_date_deadline = fields.Date(string='Deadline')
+    activity_type_id = fields.Many2one('mail.activity.type', string='Activity Type')
+    activity_user_id = fields.Many2one('res.users', string='Assigned to')
+    activity_summary = fields.Text(string='Summary')
