074309cb4452d1f8ad3aa52215dee5cd067484a1
Oddos Inc. HQ <145717033+OddosIncHQ@users.noreply.github.com>
2024-07-11 23:28:27 -0400

    Update sii_data.py

--- addons/sii_data/models/sii_data.py
+++ addons/sii_data/models/sii_data.py
@@ -5,6 +5,7 @@ class SiiData(models.Model):
     _name = 'sii.data'
     _description = 'Datos extraídos del SII'
 
+    name = fields.Char(string='Name', required=True)
     partner_id = fields.Many2one('res.partner', string='Contacto', ondelete='cascade')
     tipo_contribuyente = fields.Char(string='Tipo de Contribuyente')
     tipo = fields.Char(string='Tipo')
@@ -20,23 +21,4 @@ class SiiData(models.Model):
 
             partner = None
             for i, line in enumerate(data):
-                if i == 0:  # Asumimos que la primera línea tiene el VAT
-                    vat = line[1]
-                    partner = self.env['res.partner'].search([('vat', '=', vat)], limit=1)
-                    if not partner:
-                        partner = self.env['res.partner'].create({'name': 'Unknown', 'vat': vat})
-                elif i > 0 and line[0].lower() != 'formularios presentados':
-                    # Crear el registro de datos del SII
-                    self.create({
-                        'partner_id': partner.id,
-                        'tipo_contribuyente': line[1],
-                        'tipo': line[3],
-                        'giro_informado': line[5],
-                        'codigo_actividad_informado': line[7],
-                        'oficina_tramites': line[9],
-                    })
-
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-
-    sii_data_ids = fields.One2many('sii.data', 'partner_id', string='Datos del SII')
+                if i == 0:  # Asumimos que la primera línea tiene el VA
