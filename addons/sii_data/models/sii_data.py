3841fe5f5046dc5ee86b0db408cb9cfaeaf7f8e8
Oddos Inc. HQ <145717033+OddosIncHQ@users.noreply.github.com>
2024-07-11 23:25:32 -0400

    Update sii_data.py

--- addons/sii_data/models/sii_data.py
+++ addons/sii_data/models/sii_data.py
@@ -1,45 +1,42 @@
+import csv
 from odoo import models, fields, api
 
 class SiiData(models.Model):
     _name = 'sii.data'
-    _description = 'SII Data'
+    _description = 'Datos extraídos del SII'
 
-    name = fields.Char(string='Name', required=True)
-    vat_cl = fields.Char(string='RUT')
-    sii_pass = fields.Char(string='SII Pass')
-    email = fields.Char(string='Email')
-    phone = fields.Char(string='Phone')
-    company_id = fields.Many2one('res.company', string='Company')
-    address = fields.Char(string='Address')
-    city = fields.Char(string='City')
-    state_id = fields.Many2one('res.country.state', string='State')
-    country_id = fields.Many2one('res.country', string='Country')
-    zip = fields.Char(string='ZIP')
-    active = fields.Boolean(string='Active', default=True)
-    
-    # Campos adicionales necesarios
-    activity_type_id = fields.Many2one('mail.activity.type', string='Activity Type')
-    activity_date_deadline = fields.Date(string='Deadline')
-    activity_user_id = fields.Many2one('res.users', string='Assigned to')
-    activity_summary = fields.Text(string='Summary')
-    activity_ids = fields.One2many('mail.activity', 'res_id', string='Activities', domain=[('res_model', '=', 'sii.data')])
-    
-    # Campos relacionados con los mensajes y seguidores
-    message_follower_ids = fields.Many2many('res.partner', 'sii_data_follower_rel', 'sii_data_id', 'partner_id', string='Followers')
-    message_partner_ids = fields.Many2many('res.partner', 'sii_data_partner_rel', 'sii_data_id', 'partner_id', string='Partners')
-    message_ids = fields.One2many('mail.message', 'res_id', string='Messages', domain=[('model', '=', 'sii.data')])
-    rating_ids = fields.One2many('rating.rating', 'res_id', string='Ratings', domain=[('res_model', '=', 'sii.data')])
-    website_message_ids = fields.One2many('mail.message', 'res_id', string='Website Messages', domain=[('model', '=', 'sii.data')])
-    
-    # Campos personalizados adicionales
-    x_studio_partner_id = fields.Many2one('res.partner', string='Partner')
-    x_studio_partner_phone = fields.Char(string='Partner Phone')
-    x_studio_partner_email = fields.Char(string='Partner Email')
-    
-    upload_file = fields.Binary(string='Upload File')
-    upload_file_filename = fields.Char(string='File Name')
+    partner_id = fields.Many2one('res.partner', string='Contacto', ondelete='cascade')
+    tipo_contribuyente = fields.Char(string='Tipo de Contribuyente')
+    tipo = fields.Char(string='Tipo')
+    giro_informado = fields.Char(string='Giro Informado')
+    codigo_actividad_informado = fields.Char(string='Código de Actividad Informado')
+    oficina_tramites = fields.Char(string='Oficina para Trámites Presenciales')
 
     @api.model
-    def action_upload_file(self):
-        # Lógica para la acción del botón "Upload File"
-        pass
+    def import_csv(self, file_path):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            data = list(reader)
+
+            partner = None
+            for i, line in enumerate(data):
+                if i == 0:  # Asumimos que la primera línea tiene el VAT
+                    vat = line[1]
+                    partner = self.env['res.partner'].search([('vat', '=', vat)], limit=1)
+                    if not partner:
+                        partner = self.env['res.partner'].create({'name': 'Unknown', 'vat': vat})
+                elif i > 0 and line[0].lower() != 'formularios presentados':
+                    # Crear el registro de datos del SII
+                    self.create({
+                        'partner_id': partner.id,
+                        'tipo_contribuyente': line[1],
+                        'tipo': line[3],
+                        'giro_informado': line[5],
+                        'codigo_actividad_informado': line[7],
+                        'oficina_tramites': line[9],
+                    })
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    sii_data_ids = fields.One2many('sii.data', 'partner_id', string='Datos del SII')
