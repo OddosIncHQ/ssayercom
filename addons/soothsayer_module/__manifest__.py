4db43be239432149932b5450956d21a2498843b0
Oddos Inc. HQ <145717033+OddosIncHQ@users.noreply.github.com>
2024-07-11 22:23:17 -0400

    Update __manifest__.py

--- addons/soothsayer_module/__manifest__.py
+++ addons/soothsayer_module/__manifest__.py
@@ -5,10 +5,11 @@
     'author': 'Your Name',
     'depends': ['base', 'contacts', 'mojo_customs'],  # Ensure mojo_customs is the correct dependency
     'data': [
-        'views/main_model_views.xml',
-        'views/form_model_views.xml',
-        'data/data.xml',
-    ],
+    'views/main_model_views.xml',
+    'views/form_model_views.xml',
+    'views/csv_import_wizard_views.xml',
+    'data/data.xml',
+],
     'installable': True,
     'application': True,
 }
