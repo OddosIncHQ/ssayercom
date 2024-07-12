9c840a402515b9ff84175a4ff6364bb95a94482c
Oddos Inc. HQ <145717033+OddosIncHQ@users.noreply.github.com>
2024-07-11 22:10:22 -0400

    Create __manifest__.py

---
+++ addons/soothsayer_module/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'SoothSayer Module',
+    'version': '1.0',
+    'summary': 'Module for uploading and storing CSV data from SoothSayer',
+    'author': 'Your Name',
+    'depends': ['base', 'contacts', 'mojo_customs'],  # Ensure mojo_customs is the correct dependency
+    'data': [
+        'views/main_model_views.xml',
+        'views/form_model_views.xml',
+        'data/data.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
