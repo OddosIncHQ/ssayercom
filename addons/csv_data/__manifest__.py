c0e4e5379eeebcc804173922651f20247e3f0adb
Oddos Inc. HQ <145717033+OddosIncHQ@users.noreply.github.com>
2024-07-12 13:29:44 -0400

    Update __manifest__.py

--- addons/csv_data/__manifest__.py
+++ addons/csv_data/__manifest__.py
@@ -3,7 +3,7 @@
     'version': '1.0',
     'summary': 'Module to import and store CSV data',
     'description': 'Custom module to import CSV data and store it in respective fields for historical tracking.',
-    'author': 'Your Name',
+    'author': 'SoothSayer Techs',
     'category': 'Custom',
     'depends': ['base'],
     'data': [
