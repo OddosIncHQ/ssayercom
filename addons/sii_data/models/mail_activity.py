c814ba54d434f4faf84d900dfda11e22b218561a
Oddos Inc. HQ <145717033+OddosIncHQ@users.noreply.github.com>
2024-07-13 16:31:07 -0400

    Delete addons/sii_data directory

--- addons/sii_data/models/mail_activity.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, api
-
-class MailActivity(models.Model):
-    _inherit = 'mail.activity'
-
-    activity_date_deadline = fields.Date(string='Deadline')
-    activity_type_id = fields.Many2one('mail.activity.type', string='Activity Type')
-    activity_user_id = fields.Many2one('res.users', string='Assigned to')
-    activity_summary = fields.Text(string='Summary')
