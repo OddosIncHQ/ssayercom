2c23d841bc3e280b637e359766e61067c4751b52
Oddos Inc. HQ <145717033+OddosIncHQ@users.noreply.github.com>
2024-07-13 15:37:22 -0400

    Delete addons/contact_custom_fields directory

--- addons/contact_custom_fields/contact_custom_fields/models/res_partner.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-
-    rut_cl = fields.Char(string='RUT', required=True)
-    sii_pass = fields.Char(string='SII Pass', required=True)
